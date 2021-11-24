051e9627a290a68463f84ba17ac70e8a8a64e246
Ishaan Verma <63767298+IshaanVerma1130@users.noreply.github.com>
2021-11-24 20:46:02 +0530

    Contact Context

--- src/components/NewConversationModal.jsx
+++ src/components/NewConversationModal.jsx
@@ -1,7 +1,27 @@
 import React from "react";
+import { Modal, Button, Form } from "react-bootstrap";
 
 function NewConversationModal() {
-  return <div></div>;
+  return (
+    <>
+      <Modal.Header closeButton>Create Contact</Modal.Header>
+      <Modal.Body>
+        <Form onSubmit={handleSubmit}>
+          <Form.Group>
+            <Form.Label>ID</Form.Label>
+            <Form.Control type="text" ref={idRef} required />
+          </Form.Group>
+          <Form.Group className="mt-2">
+            <Form.Label>Name</Form.Label>
+            <Form.Control type="text" ref={nameRef} required />
+          </Form.Group>
+          <Button className="mt-4" type="submit">
+            Create
+          </Button>
+        </Form>
+      </Modal.Body>
+    </>
+  );
 }
 
 export default NewConversationModal;
